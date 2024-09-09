e9b42f371a0753a7215f91f44159ba360c178bd2
Alex Salmeron <juanasalmeron95@gmail.com>
2024-09-09 12:40:20 -0400

    buggy code version

---
+++ the-event-planner.py
@@ -0,0 +1,7 @@
+#Task 1: Code Correction You are provided with a Python script that is supposed to help in event planning, but it has errors. Identify and fix them.
+
+#BUGGY CODE
+
+attendees = input("Enter number of attendees: ")
+venue = "large hall" if attendees > 100 else "conference room"
+print(venue)
