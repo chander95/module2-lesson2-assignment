9daaa2ca6092ded27c04b809ea8c3f646352a921
Alex Salmeron <juanasalmeron95@gmail.com>
2024-09-09 12:30:15 -0400

    task 1 in question 1 completed. Buggy code fixed.

--- the-adventure-game.py
+++ the-adventure-game.py
@@ -1,14 +1,14 @@
 # Task 1: Code Correction You are provided with a Python script that is supposed to guide a user through an adventure game, but it has some errors. Identify and fix them.
 
-#BUGGY CODE
+#FIXED CODE
 
 place = input("Choose a place: forest or cave? ")
 
-if place = "forest":
-    action = input("climb a tree or cross a river?")
-    if action = "climb a tree":
+if place == "forest":
+    action = input("climb a tree or cross a river? ")
+    if action == "climb a tree":
         print("You found a bird's nest!")
-    else action = "cross a river":
+    else: 
         print("You found a boat!")
-elif place = "cave":
+elif place == "cave":
     print("You find a hidden treasure!")
