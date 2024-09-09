2d15fd874510f7d8a722bcb27891f5e9be11ac87
Alex Salmeron <juanasalmeron95@gmail.com>
2024-09-09 12:39:14 -0400

    task 3 in question 1 completed. Pass statements are in place should the user input something invalid.

--- the-adventure-game.py
+++ the-adventure-game.py
@@ -8,14 +8,22 @@ if place == "forest":
     action = input("climb a tree or cross a river? ")
     if action == "climb a tree":
         print("You found a bird's nest!")
-    else: 
+    elif action == "cross a river": 
         print("You found a boat!")
+    else:
+        pass
 elif place == "cave":
     action = input("light a torch or proceed in the dark? ")
     if action == "light a torch":
         print("You found a hidden treasure!")
-    else:
+    elif action == "proceed in the dark":
         print("Beware of the what lurks in the dark...")
+    else:
+        pass
+else:
+    pass
 
 
 # Task 2: Based on your corrected code from Task 1, expand the game. If the user chooses "cave", ask them if they want to "light a torch" or "proceed in the dark", and provide outcomes for each decision.
+
+#Task 3: If the user makes an invalid choice at any point, incorporate a pass statement to handle it. HINT: How can an else statement be of use here?
