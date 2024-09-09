42240eb60ec2bf12238d86a0f20a02f14fad1c25
Alex Salmeron <juanasalmeron95@gmail.com>
2024-09-09 12:50:47 -0400

    task 2 Q2 completed.

--- the-event-planner.py
+++ the-event-planner.py
@@ -4,4 +4,8 @@
 
 attendees = int(input("Enter number of attendees: "))
 venue = "large hall" if attendees > 100 else "conference room"
+additionals = "audio system" if venue == "large hall" else "projector"
 print(venue)
+print(additionals)
+
+#Task 2: Based on the corrected code from Task 1, further enhance your code to recommend additional things like "audio system" or "projector" based on the number of attendees.
